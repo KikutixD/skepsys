a077705229a8d79c106d9eea818d15cbe8b76a0f
Phelipe Prieto <phelipe.prieto@skepsys.com.br>
2018-05-28 15:15:40 -0300

    Projeto Criado

---
+++ src/main/java/com/phelipeprieto/skepsys/SkepsysApplication.java
@@ -0,0 +1,12 @@
+package com.phelipeprieto.skepsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkepsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkepsysApplication.class, args);
+	}
+}
