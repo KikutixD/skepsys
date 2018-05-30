0fc724279b389cde3a399b712434b0ad7c31d171
Phelipe Prieto <phelipe.prieto@skepsys.com.br>
2018-05-30 15:39:59 -0300

    ajustes no endpoint /categorias/{id}

--- src/main/java/com/phelipeprieto/skepsys/services/CategoriaService.java
+++ src/main/java/com/phelipeprieto/skepsys/services/CategoriaService.java
@@ -7,15 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.phelipeprieto.skepsys.domain.Categoria;
 import com.phelipeprieto.skepsys.repositories.CategoriaRepository;
+import com.phelipeprieto.skepsys.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
-	
+		
 	public Categoria find(Integer id_categoria) {
 		Optional<Categoria> obj = repo.findById(id_categoria);
-		return obj.orElse(null);
-	}
-}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Código: " + id_categoria + ", Tipo: " + Categoria.class.getName()));
+		}
+	
+ }
+
