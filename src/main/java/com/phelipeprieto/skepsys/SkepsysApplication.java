ccce4dac0cd6ad33fcf3a3b770483e95a5d720e4
Phelipe Prieto <phelipe.prieto@skepsys.com.br>
2018-05-30 14:11:24 -0300

    Criando operacao de isntanciacao

--- src/main/java/com/phelipeprieto/skepsys/SkepsysApplication.java
+++ src/main/java/com/phelipeprieto/skepsys/SkepsysApplication.java
@@ -1,12 +1,35 @@
 package com.phelipeprieto.skepsys;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SkepsysApplication {
+import com.phelipeprieto.skepsys.domain.Categoria;
+import com.phelipeprieto.skepsys.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SkepsysApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SkepsysApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Sistemas Web");
+		Categoria cat2 = new Categoria(null, "Sistemas Desk");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
 }
